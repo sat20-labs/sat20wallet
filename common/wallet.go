632dccdda42dde2ec974e9181bad43da96001efe
sharphill <cszyf@163.com>
2025-01-15 22:53:55 +0800

    modified

--- common/wallet.go
+++ common/wallet.go
@@ -2,16 +2,12 @@ package common
 
 import (
 	"github.com/btcsuite/btcd/btcutil/psbt"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 
 	"github.com/sat20-labs/satsnet_btcd/btcec"
 	"github.com/sat20-labs/satsnet_btcd/btcec/ecdsa"
 
 	spsbt "github.com/sat20-labs/satsnet_btcd/btcutil/psbt"
-	stxscript "github.com/sat20-labs/satsnet_btcd/txscript"
-	swire "github.com/sat20-labs/satsnet_btcd/wire"
 )
 
 type Wallet interface {
@@ -24,23 +20,8 @@ type Wallet interface {
 	GetNodePubKey() *secp256k1.PublicKey
 	GetPaymentPubKey() *secp256k1.PublicKey
 
-	SignTxInput(tx *wire.MsgTx, prevFetcher txscript.PrevOutputFetcher,
-		sigHashes *txscript.TxSigHashes,
-		index int, witnessScript []byte) ([]byte, error)
-	SignTxInput_SatsNet(tx *swire.MsgTx, prevFetcher stxscript.PrevOutputFetcher,
-		sigHashes *stxscript.TxSigHashes,
-		index int, witnessScript []byte) ([]byte, error)
-	SignTx(tx *wire.MsgTx, prevFetcher txscript.PrevOutputFetcher) error
-	SignTx_SatsNet(tx *swire.MsgTx, prevFetcher stxscript.PrevOutputFetcher) error
-	SignTxWithPeer(tx *wire.MsgTx, prevFetcher txscript.PrevOutputFetcher,
-		witnessScript []byte, peerPubKey []byte, peerSig [][]byte) ([][]byte, error)
-	SignTxWithPeer_SatsNet(tx *swire.MsgTx, prevFetcher stxscript.PrevOutputFetcher,
-		witnessScript []byte, peerPubKey []byte, peerSig [][]byte) ([][]byte, error)
-	PartialSignTx(tx *wire.MsgTx, prevFetcher txscript.PrevOutputFetcher,
-		witnessScript []byte, pos int) ([][]byte, error)
-	PartialSignTx_SatsNet(tx *swire.MsgTx, prevFetcher stxscript.PrevOutputFetcher,
-		witnessScript []byte, pos int) ([][]byte, error)
+
 	SignMessage(msg []byte) (*ecdsa.Signature, error)
-	SignPsbt(packet *psbt.Packet) error
+	SignPsbt(packet *psbt.Packet) (error)
 	SignPsbt_SatsNet(packet *spsbt.Packet) error
 }
